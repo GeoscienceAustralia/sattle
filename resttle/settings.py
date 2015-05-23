8531cab79b490757b1e1ec3a7b8f40ae8f15bc79
Fei Zhang <fei.zhang@ga.gov.au>
2015-05-23 21:29:57 +1000

    from my ubuntu. Use rms  db instead of unix login user

--- resttle/settings.py
+++ resttle/settings.py
@@ -82,16 +82,16 @@ DATABASES = {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     # },
-    'default': {
+    'default-1': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         #'NAME': 'rmsdb',
         'NAME': 'resttle',
     },
-    'postgres':{
+    'default':{
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'mydatabase',
-        'USER': 'mydatabaseuser',
-        'PASSWORD': 'mypassword',
+        'NAME': 'rmsdb',
+        'USER': 'rms',
+        'PASSWORD': 'daf',
         'HOST': '127.0.0.1',
         'PORT': '5432',
 
---
+++ tleserv/test/tle_crud.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+__author__ = 'fzhang'
+import os
+import sys
+import django
+
+if __name__ == "__main__":
+    # setup django project
+    os.sys.path.append("/home/fzhang/PycharmProjects/resttle")
+
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "resttle.settings")
+
+    django.setup()
+    ################################################
+    from tleserv.models import Satellite
+    from tleserv.models import Tle
+
+    print "Active Satellites ......"
+    sat2 = Satellite.objects.filter(isactive='True')
+    print sat2
+
+    print "active satellite with given norad number"
+
+    #sat_inst= Satellite.objects.filter(norad_number=37849)
+    sat_inst = sat2.filter(norad_number=37849)
+    print type(sat_inst)
+
+####
+    print Tle.objects.all()
+
+    a_tle = Tle()
+    a_tle.line1 = "1 37849U 11061A   15138.00000000  .00000000  00000-0  45447-5 2    02"
+    a_tle.line2 = "2 37849 098.6970 077.8602 0001252 092.0306 348.7111 14.19552114184104"
+
+    a_tle.norad_number = sat_inst[0]
+    a_tle.status = True
+    a_tle.epochsec = 1431907200.0000000000
+    a_tle.md5sum="97d1bdc6f3a150977d7eeefdea56723c"
+    a_tle.path2file = "/data/ephemeris/source/nasa/20150519.drl.tle"
+    a_tle.tle_dt_utc = "2015-05-18 00:00:00+00"
+    a_tle.tle_source = 0
+    a_tle.inp_dt_utc="2015-05-19 10:40:02.685394+00"
+
+    a_tle.save()
+
+    print a_tle.tleid
