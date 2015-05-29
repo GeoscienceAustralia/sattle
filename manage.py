5baca858de73dad7b320c7808902bc49394179e3
Fei Zhang <fei.zhang@ga.gov.au>
2015-05-29 14:17:37 +1000

    Update manage.py
    updates

--- manage.py
+++ manage.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python
+#  python2.7 with django* installed
+#  To test run the Web server
+#  ./manage.py runserver 0.0.0.0:8000
+
 import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sattle.settings")
+    # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sattle.settings")
+    # More specifically
+    os.environ["DJANGO_SETTINGS_MODULE"] = "sattle.settings"
 
     from django.core.management import execute_from_command_line
 
